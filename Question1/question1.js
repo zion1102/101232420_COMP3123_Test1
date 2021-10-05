ef58898a027249f5843d9d25f048081af47c240e
zion1102 <78486908+zion1102@users.noreply.github.com>
2021-10-05 17:14:22 -0400

    Update question1.js

--- Question1/question1.js
+++ Question1/question1.js
@@ -1,30 +1,5 @@
 const mixedArray = ['PIZZA',10,true,25,false,'Wings']
 
-/*
-
-function lowerCaseWords(array){
-    var newArr =[]
-
-    return  new Promise(function(resolve, reject){
-        for (let i = 0; i<array.length;i++){
-            if (typeof array[i] == 'string'){
-                newArr.push(" "+array[i].toLowerCase())
-               
-            }
-
-        }
-        resolve(`${newArr}`)
-
-        
-    })
-        
-    
-}
-
-
-
-console.log(lowerCaseWords(mixedArray))
-*/
 
 function lowerCaseWords(array){
     return new Promise(function(resolve, reject){
